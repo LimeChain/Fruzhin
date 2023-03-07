55cf4192654033c89e7e077397571805a33b7b02
Boris Velkovski <boris@Boriss-MacBook-Pro.local>
2023-03-07 10:07:20 +0200

    Refactor HostConfig imports

--- src/main/java/com/limechain/config/HostConfig.java
+++ src/main/java/com/limechain/config/HostConfig.java
@@ -2,7 +2,13 @@ package com.limechain.config;
 
 import com.limechain.chain.Chain;
 import com.limechain.storage.RocksDBInitializer;
-import org.apache.commons.cli.*;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 import java.io.IOException;
 import java.util.Properties;
