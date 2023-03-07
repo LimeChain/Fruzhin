8e27fc7bdae3efdda91f896bfe281f8ebd5e3242
Boris Velkovski <boris@Boriss-MacBook-Pro.local>
2023-03-07 16:18:16 +0200

    Add lombok to system properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 
     compileOnly("org.projectlombok:lombok:1.18.8")
     implementation("org.projectlombok:lombok:1.18.22")
-    
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 
--- src/main/java/com/limechain/rpc/methods/system/dto/SystemProperties.java
+++ src/main/java/com/limechain/rpc/methods/system/dto/SystemProperties.java
@@ -1,5 +1,10 @@
 package com.limechain.rpc.methods.system.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
 public class SystemProperties {
     private Integer ss58Format;
     private Integer tokenDecimals;
