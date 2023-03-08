b42806a22cc0170b5d4f0db57fc987a90d4e8def
vikinatora <vikinatora@gmail.com>
2023-03-08 13:57:13 +0200

    Fix RocksDB tests crashing

--- src/test/java/com/limechain/storage/StorageTest.java
+++ src/test/java/com/limechain/storage/StorageTest.java
@@ -1,15 +1,27 @@
 package com.limechain.storage;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.rocksdb.RocksDB;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class StorageTest {
+    private RocksDB db;
+
+    @BeforeEach
+    public void setup() {
+        db = RocksDBInitializer.initializeTestDatabase();
+    }
+
+    @AfterEach
+    public void close() {
+        RocksDBInitializer.closeInstances();
+    }
 
     @Test
     public void initializeStorageTest() {
-        RocksDB db = RocksDBInitializer.initializeTestDatabase();
         assertNotNull(db);
         db.close();
     }
