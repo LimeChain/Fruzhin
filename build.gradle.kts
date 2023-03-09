386471820cba7f06fcd7c95d62a48755aa2bb0d3
Georgi Georgiev <georgi.getz@outlook.com>
2023-03-09 13:15:44 +0200

    copy main to dev (#37)
    * added a checkstyle and filled it with rules
    
    Signed-off-by: Georgi Georgiev <georgi.getz@outlook.com>
    
    * updated checkstyle rules and location
    
    Signed-off-by: Georgi Georgiev <georgi.getz@outlook.com>
    
    ---------
    
    Signed-off-by: Georgi Georgiev <georgi.getz@outlook.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,53 @@
 plugins {
     id("java")
+    // Can't use newest 3.x version of Spring Boot as it fails to build with jsonprc4j
+    // See: https://stackoverflow.com/questions/74760350/application-fails-on-run-in-a-new-release-of-springboot-3-0-0
+    id("org.springframework.boot") version "2.7.3"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("application")
+    id("checkstyle")
 }
 
-group = "org.limechain"
+application {
+    mainClass.set("com.limechain.Main")
+}
+
+checkstyle {
+    config = resources.text.fromFile("checkstyle.xml")
+}
+
+group = "com.limechain"
 version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.rocksdb:rocksdbjni:7.8.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+
+    // Jackson
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+
+    // CLI
+    implementation("commons-cli:commons-cli:1.3.1")
+
+    // JSON-RPC dependencies
+    // TODO: Publish imported packages to mvnrepository and import them
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    // Needed for strange error when starting up json rpc server
+    // See: https://github.com/briandilley/jsonrpc4j/issues/280
+    implementation("javax.jws:javax.jws-api:1.1")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Websockets
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
 }
 
 tasks.getByName<Test>("test") {
