9d97a7334128aeafd8752bdb0b3fc48f258e8871
Boris Velkovski <boris@Boriss-MacBook-Pro.local>
2023-03-10 13:33:57 +0200

    Fix lint

--- src/test/java/com/limechain/ExampleTest.java
+++ src/test/java/com/limechain/ExampleTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ExampleTest {
     @Test
-    void TestIntAddition() {
+    void testIntAddition() {
         assertEquals(5, intAddition(2, 3));
     }
 }
