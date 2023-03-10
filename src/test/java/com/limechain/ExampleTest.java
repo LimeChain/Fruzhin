21f8cf5185f7c52b190d68e4347234a7a38d0801
Boris Velkovski <boris@Boriss-MacBook-Pro.local>
2023-03-10 14:07:09 +0200

    Reorder imports

--- src/test/java/com/limechain/ExampleTest.java
+++ src/test/java/com/limechain/ExampleTest.java
@@ -1,8 +1,8 @@
 package com.limechain;
 
 import org.junit.jupiter.api.Test;
-import static com.limechain.Example.intAddition;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.limechain.Example.intAddition;
 
 class ExampleTest {
     @Test
