a003939367fa66d16f543c432dd3b89142c405c0
Boris Velkovski <boris@Boriss-MacBook-Pro.local>
2023-03-30 16:21:18 +0300

    Malformed address fix

--- src/main/java/com/limechain/Main.java
+++ src/main/java/com/limechain/Main.java
@@ -3,12 +3,15 @@ package com.limechain;
 import com.limechain.lightclient.LightClient;
 import com.limechain.rpc.http.server.HttpRpc;
 import com.limechain.rpc.ws.server.WebSocketRPC;
+import org.peergos.protocol.dnsaddr.DnsAddr;
 
 public class Main {
     public static void main(String[] args) {
-        HttpRpc httpRpc = new HttpRpc();
-        WebSocketRPC wsRpc = new WebSocketRPC();
-        LightClient client = new LightClient(args, httpRpc, wsRpc);
-        client.start();
+//        HttpRpc httpRpc = new HttpRpc();
+//        WebSocketRPC wsRpc = new WebSocketRPC();
+//        LightClient client = new LightClient(args, httpRpc, wsRpc);
+//        client.start();
+        var dadr = DnsAddr.resolve("/dnsaddr/0.westend.paritytech.net/tcp/30333/p2p/12D3KooWKer94o1REDPtAhjtYR4SdLehnSrN8PEhBnZm5NBoCrMC");
+        System.out.println(dadr);
     }
 }
