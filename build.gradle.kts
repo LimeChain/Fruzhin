a195f1fc44915d57dd6e3727e54fedb59085b2da
vikinatora <vikinatora@gmail.com>
2023-03-30 13:38:47 +0300

    Remove unnecessary dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-websocket")
 
     implementation("org.java-websocket:Java-WebSocket:1.5.3")
-    implementation("com.google.protobuf:protobuf-java:3.22.2")
 
 }
 
