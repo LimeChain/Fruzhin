1bc2c27601807a9b30629a85e297c931bdfd8444
Boris Velkovski <boris@Boriss-MacBook-Pro.local>
2023-04-03 13:52:42 +0300

    Remove unused import

--- src/main/java/com/limechain/Main.java
+++ src/main/java/com/limechain/Main.java
@@ -3,7 +3,6 @@ package com.limechain;
 import com.limechain.lightclient.LightClient;
 import com.limechain.rpc.http.server.HttpRpc;
 import com.limechain.rpc.ws.server.WebSocketRPC;
-import org.peergos.protocol.dnsaddr.DnsAddr;
 
 public class Main {
     public static void main(String[] args) {
