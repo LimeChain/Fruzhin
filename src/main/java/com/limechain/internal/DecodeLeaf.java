68fe0a909ab98a990d0b989669b445c260735f5f
vikinatora <vikinatora@gmail.com>
2023-06-02 18:00:30 +0300

    Fix `decodeKey`

--- src/main/java/com/limechain/internal/DecodeLeaf.java
+++ src/main/java/com/limechain/internal/DecodeLeaf.java
@@ -11,11 +11,12 @@ public class DecodeLeaf {
             }
 
             int keySize = partialKeyLength / 2 + partialKeyLength % 2;
-            byte[] key = reader.readByteArray();
+            byte[] key = reader.readByteArray(keySize);
             if (keySize != key.length) {
                 throw new TrieDecoderException("Read bytes is not equal to key size. Read " +
                         key + " bytes, expected " + key.length);
             }
+            // Maybe we will have to return only [partialKeyLength%2:]
             return LittleEndianUtils.convertBytes(key);
         } catch (IndexOutOfBoundsException error) {
             throw new TrieDecoderException("Could not decode partial key: " + error.getMessage());
