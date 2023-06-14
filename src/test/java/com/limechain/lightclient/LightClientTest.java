8fe53880ac8f7da2b626275eb2fa2d05b5bad4bd
boris <bobivelkdev@gmail.com>
2023-06-14 17:51:08 +0300

    Fix build

--- src/test/java/com/limechain/lightclient/LightClientTest.java
+++ src/test/java/com/limechain/lightclient/LightClientTest.java
@@ -70,8 +70,5 @@ public class LightClientTest {
 
         verify(httpRpc, times(1)).stop();
         verify(wsRpc, times(1)).stop();
-        verify(network, times(1)).stop();
-        verify(warpSync, times(1)).stop();
-
     }
 }
