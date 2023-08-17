0d077ed79a6faf329a96381cbfc142d4da963bd8
Мurаd Hаmzа <murad.hamza@limechain.tech>
2023-08-17 15:59:24 +0300

    chore: remove forgotten sysout

--- src/test/java/com/limechain/utils/Ed25519UtilsTest.java
+++ src/test/java/com/limechain/utils/Ed25519UtilsTest.java
@@ -12,7 +12,7 @@ class Ed25519UtilsTest {
 
     private static final String PEER_ID = "12D3KooWKsULPNyZ4zDy1dcBhEsticfvYQgQUWiFNXbcPLefJ9Zd";
     private static final byte[] PRIVATE_KEY_BYTE = new byte[] {109, -89, 75, 125, 88, 37, -93, 89, 11, -93, -76, -39,
-            111, -33, -101, -51, -19, 63, 124, -90, 40, -33, 119, -108, 91, 127, 123, -54, -110, -89, -11, 10, };
+            111, -33, -101, -51, -19, 63, 124, -90, 40, -33, 119, -108, 91, 127, 123, -54, -110, -89, -11, 10};
 
     @Test
     void generatesPrivateKey() {
@@ -32,7 +32,6 @@ class Ed25519UtilsTest {
         assertNotNull(ed25519PrivateKey);
         assertNotNull(ed25519PrivateKey.publicKey());
         assertNotNull(peerId);
-        System.out.println(peerId.toBase58());
         assertEquals(PEER_ID, peerId.toBase58());
     }
 
