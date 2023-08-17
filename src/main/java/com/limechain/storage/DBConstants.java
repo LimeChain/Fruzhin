0d077ed85101445e387b1a7d96981b80034e8f15
Мurаd Hаmzа <murad.hamza@limechain.tech>
2023-08-17 15:33:54 +0300

    docs: fix formatting and add missing

--- src/main/java/com/limechain/storage/DBConstants.java
+++ src/main/java/com/limechain/storage/DBConstants.java
@@ -1,8 +1,12 @@
 package com.limechain.storage;
 
 public class DBConstants {
-    /*
-    * Key for storing the privateKey for nabu */
+    /**
+    * Key for storing the privateKey for nabu
+    *  */
     public static final String PEER_ID = "nodePeerId";
+    /**
+     * Key under which the genesis chain spec is stored
+     */
     public static final String GENESIS_KEY = "genesis";
 }
