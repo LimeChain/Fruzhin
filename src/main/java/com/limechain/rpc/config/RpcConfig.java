0d077ed0cabfb755d749da9171915fed4f9a35b5
Мurаd Hаmzа <murad.hamza@limechain.tech>
2023-08-17 11:43:01 +0300

    docs: the RpcConfig

--- src/main/java/com/limechain/rpc/config/RpcConfig.java
+++ src/main/java/com/limechain/rpc/config/RpcConfig.java
@@ -1,11 +1,19 @@
 package com.limechain.rpc.config;
 
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.socket.server.support.WebSocketHandlerMapping;
 
+/**
+ * Spring configuration class used to configure the websocket handler mapping to process only websocket requests or http requests with upgrade headers only
+ *
+ * <p>
+ *     The configuration is in separate file from {@link CommonConfig} because of the circular dependency loop
+ */
 @Configuration
+@Log
 public class RpcConfig {
 
     public RpcConfig(@Qualifier("webSocketHandlerMapping") HandlerMapping webSocketHandlerMapping) {
