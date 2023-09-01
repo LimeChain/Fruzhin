9f17d87b74f07aee6346042caf7a36bfed13cf98
Stoyan Stoyanov <stoyan.stoyanov@limechain.tech>
2023-09-01 17:46:27 +0300

    Refactor BlockAnnounce classes to match Grandpa

--- src/main/java/com/limechain/network/protocol/blockannounce/BlockAnnounceController.java
+++ src/main/java/com/limechain/network/protocol/blockannounce/BlockAnnounceController.java
@@ -1,5 +1,16 @@
 package com.limechain.network.protocol.blockannounce;
 
-public interface BlockAnnounceController {
-    void sendHandshake();
+import io.libp2p.core.Stream;
+
+public class BlockAnnounceController {
+    protected BlockAnnounceEngine engine = new BlockAnnounceEngine();
+    protected final Stream stream;
+
+    public BlockAnnounceController(Stream stream) {
+        this.stream = stream;
+    }
+
+    public void sendHandshake() {
+        engine.writeHandshakeToStream(stream, stream.remotePeerId());
+    }
 }
