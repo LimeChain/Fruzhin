0d077ed89bc0f5f5eb0f1a423dba5bd1d85a4344
Мurаd Hаmzа <murad.hamza@limechain.tech>
2023-09-26 11:42:23 +0300

    feat: Send state sync request

--- src/main/java/com/limechain/network/protocol/sync/SyncProtocol.java
+++ src/main/java/com/limechain/network/protocol/sync/SyncProtocol.java
@@ -1,13 +1,18 @@
 package com.limechain.network.protocol.sync;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.limechain.network.protocol.sync.pb.SyncMessage;
 import io.libp2p.core.Stream;
 import io.libp2p.protocol.ProtocolHandler;
 import io.libp2p.protocol.ProtocolMessageHandler;
-import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+import lombok.extern.java.Log;
+import org.apache.tomcat.util.buf.HexUtils;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
@@ -21,25 +26,35 @@ public class SyncProtocol extends ProtocolHandler<SyncController> {
         super(MAX_REQUEST_SIZE, MAX_RESPONSE_SIZE);
     }
 
-    @Override
-    protected CompletableFuture<SyncController> onStartInitiator(Stream stream) {
+    @NotNull
+    private static <T extends ProtocolMessageHandler<ByteBuf> & SyncController> CompletableFuture<SyncController>
+    onStartStream(Stream stream, T handler) {
         stream.pushHandler(new ProtobufVarint32FrameDecoder());
-        stream.pushHandler(new ProtobufDecoder(SyncMessage.BlockResponse.getDefaultInstance()));
 
         stream.pushHandler((new ProtobufVarint32LengthFieldPrepender()));
         stream.pushHandler((new ProtobufEncoder()));
 
-        Sender handler = new Sender(stream);
         stream.pushHandler(handler);
         return CompletableFuture.completedFuture(handler);
     }
 
+    @Override
+    protected CompletableFuture<SyncController> onStartInitiator(@NotNull Stream stream) {
+        return onStartStream(stream, new Sender(stream));
+    }
+
+    @Override
+    protected CompletableFuture<SyncController> onStartResponder(@NotNull Stream stream) {
+        return onStartStream(stream, new Receiver(stream));
+    }
+
     // Class for handling outgoing requests
+    @Log
     static class Sender
-            implements ProtocolMessageHandler<SyncMessage.BlockResponse>,
+            implements ProtocolMessageHandler<ByteBuf>,
             SyncController {
         public static final int MAX_QUEUE_SIZE = 50;
-        private final LinkedBlockingDeque<CompletableFuture<SyncMessage.BlockResponse>> queue =
+        private final LinkedBlockingDeque<CompletableFuture<byte[]>> queue =
                 new LinkedBlockingDeque<>(MAX_QUEUE_SIZE);
 
         private final Stream stream;
@@ -49,17 +64,40 @@ public class SyncProtocol extends ProtocolHandler<SyncController> {
         }
 
         @Override
-        public void onMessage(Stream stream, SyncMessage.BlockResponse msg) {
-            Objects.requireNonNull(queue.poll()).complete(msg);
+        public void onMessage(@NotNull Stream stream, ByteBuf msg) {
+            int length = msg.readableBytes();
+            byte[] array;
+            if (msg.hasArray()) {
+                array = msg.array();
+            } else {
+                array = ByteBufUtil.getBytes(msg, msg.readerIndex(), length, false);
+            }
+
+            Objects.requireNonNull(queue.poll()).complete(array);
             stream.closeWrite();
         }
 
         @Override
-        public CompletableFuture<SyncMessage.BlockResponse> send(SyncMessage.BlockRequest req) {
-            CompletableFuture<SyncMessage.BlockResponse> res = new CompletableFuture<>();
+        public <T, E> CompletableFuture<E> send(T req, Class<E> resClass) {
+            CompletableFuture<byte[]> res = new CompletableFuture<>();
             queue.add(res);
             stream.writeAndFlush(req);
-            return res;
+            return res.thenApply(msg -> parseResponse(msg, resClass));
+        }
+
+        private <E> E parseResponse(byte[] msg, Class<E> resClass) {
+            try {
+                if (SyncMessage.BlockResponse.class.equals(resClass)) {
+                    return (E) SyncMessage.BlockResponse.parseFrom(msg);
+                } else if (SyncMessage.StateResponse.class.equals(resClass)) {
+                    return (E) SyncMessage.StateResponse.parseFrom(msg);
+                }
+            } catch (InvalidProtocolBufferException e) {
+                log.severe("Error while parsing response: " + e.getMessage());
+                return null;
+            }
+            log.severe("Unknown response class: " + resClass);
+            return null;
         }
 
         @Override
@@ -67,5 +105,60 @@ public class SyncProtocol extends ProtocolHandler<SyncController> {
             Objects.requireNonNull(queue.poll()).completeExceptionally(cause);
         }
 
+    }
+
+    @Log
+    static class Receiver implements ProtocolMessageHandler<ByteBuf>, SyncController {
+        private final Stream stream;
+
+        public Receiver(Stream stream) {
+            this.stream = stream;
+        }
+
+        @Override
+        public void onMessage(@NotNull Stream stream, ByteBuf msg) {
+            int length = msg.readableBytes();
+            byte[] array;
+            if (msg.hasArray()) {
+                array = msg.array();
+            } else {
+                array = ByteBufUtil.getBytes(msg, msg.readerIndex(), length, false);
+            }
+
+            SyncMessage.BlockRequest blockRequest = null;
+            SyncMessage.StateRequest stateRequest = null;
+            try {
+                blockRequest = SyncMessage.BlockRequest.parseFrom(array);
+            } catch (InvalidProtocolBufferException e) {
+                //Empty catch block
+            }
+            try {
+                stateRequest = SyncMessage.StateRequest.parseFrom(array);
+            } catch (InvalidProtocolBufferException e) {
+                //Empty catch block
+            }
+
+            if (blockRequest != null) {
+                handleBlockRequest(blockRequest);
+            } else if (stateRequest != null) {
+                handleStateRequest(stateRequest);
+            } else {
+                log.severe("Received unknown request with data " + HexUtils.toHexString(array));
+            }
+        }
+
+        private void handleStateRequest(SyncMessage.StateRequest stateRequest) {
+            //Todo: Create state response
+//            SyncMessage.StateResponse stateResponse = SyncMessage.StateResponse.newBuilder().build();
+//            stream.writeAndFlush(stateResponse);
+            stream.closeWrite();
+        }
+
+        private void handleBlockRequest(SyncMessage.BlockRequest blockRequest) {
+            //Todo: create block response (must get the block from the storage)
+//            SyncMessage.BlockResponse blockResponse = SyncMessage.BlockResponse.newBuilder().build();
+//            stream.writeAndFlush(blockResponse);
+            stream.closeWrite();
+        }
     }
 }
