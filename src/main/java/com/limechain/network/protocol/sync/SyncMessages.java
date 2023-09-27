0d077edb7a74a3985108a88dc92ed6eb0c9175f5
Мurаd Hаmzа <murad.hamza@limechain.tech>
2023-09-27 10:07:58 +0300

    chore: add sample test

--- src/main/java/com/limechain/network/protocol/sync/SyncMessages.java
+++ src/main/java/com/limechain/network/protocol/sync/SyncMessages.java
@@ -2,12 +2,15 @@ package com.limechain.network.protocol.sync;
 
 import com.limechain.network.StrictProtocolBinding;
 import com.limechain.network.protocol.sync.pb.SyncMessage;
+import com.limechain.utils.StringUtils;
 import io.libp2p.core.AddressBook;
 import io.libp2p.core.Host;
 import io.libp2p.core.PeerId;
 import lombok.extern.java.Log;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 
 @Log
@@ -30,6 +33,19 @@ public class SyncMessages extends StrictProtocolBinding<SyncController> {
             log.log(Level.INFO, "Received response: " + response.toString());
             return response;
         } catch (Exception e) {
+            log.log(Level.SEVERE, "Error while sending remote call request: ", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public SyncMessage.StateResponse remoteStateRequest(Host us, AddressBook addrs, PeerId peer, String blockHash) {
+        SyncController controller = dialPeer(us, peer, addrs);
+        try {
+            SyncMessage.StateResponse resp = controller.sendStateRequest(StringUtils.remove0xPrefix(blockHash))
+                    .get(30, TimeUnit.SECONDS);
+            log.log(Level.INFO, "Received state sync response " + resp.toString());
+            return resp;
+        } catch (ExecutionException | InterruptedException | TimeoutException e) {
             log.log(Level.SEVERE, "Error while sending remote call request: ", e);
             throw new RuntimeException(e);
         }
