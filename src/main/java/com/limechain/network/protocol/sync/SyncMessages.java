0d077edc123c2f0fa32b41a3becf3cc52b809581
Мurаd Hаmzа <murad.hamza@limechain.tech>
2023-09-27 09:09:22 +0300

    chore: remove unused method

--- src/main/java/com/limechain/network/protocol/sync/SyncMessages.java
+++ src/main/java/com/limechain/network/protocol/sync/SyncMessages.java
@@ -2,15 +2,12 @@ package com.limechain.network.protocol.sync;
 
 import com.limechain.network.StrictProtocolBinding;
 import com.limechain.network.protocol.sync.pb.SyncMessage;
-import com.limechain.utils.StringUtils;
 import io.libp2p.core.AddressBook;
 import io.libp2p.core.Host;
 import io.libp2p.core.PeerId;
 import lombok.extern.java.Log;
 
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 
 @Log
@@ -33,20 +30,6 @@ public class SyncMessages extends StrictProtocolBinding<SyncController> {
             log.log(Level.INFO, "Received response: " + response.toString());
             return response;
         } catch (Exception e) {
-            log.log(Level.SEVERE, "Error while sending remote call request: ", e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    public SyncMessage.StateResponse warpSyncRequest(Host us, AddressBook addrs, PeerId peer, String blockHash) {
-        SyncController controller = dialPeer(us, peer, addrs);
-        try {
-            SyncMessage.StateResponse resp = controller.sendStateRequest(StringUtils.remove0xPrefix(blockHash))
-                    .get(30, TimeUnit.SECONDS);
-            log.log(Level.INFO, "Received warp sync response with " +
-                    resp.getProof().toByteArray().length + " fragments");
-            return resp;
-        } catch (ExecutionException | InterruptedException | TimeoutException e) {
             log.log(Level.SEVERE, "Error while sending remote call request: ", e);
             throw new RuntimeException(e);
         }
