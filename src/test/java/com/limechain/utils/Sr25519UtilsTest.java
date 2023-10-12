00d07ab0d5a59854bab80627ff9199965f3526c0
Мurаd Hаmzа <murad.hamza@limechain.tech>
2023-10-12 12:06:00 +0300

    test: basic signing and verification

--- src/test/java/com/limechain/utils/Sr25519UtilsTest.java
+++ src/test/java/com/limechain/utils/Sr25519UtilsTest.java
@@ -1,5 +1,7 @@
 package com.limechain.utils;
 
+import com.limechain.runtime.hostapi.dto.Key;
+import com.limechain.runtime.hostapi.dto.VerifySignature;
 import io.emeraldpay.polkaj.schnorrkel.Schnorrkel;
 import org.junit.jupiter.api.Test;
 import org.web3j.crypto.MnemonicUtils;
@@ -7,8 +9,23 @@ import org.web3j.crypto.MnemonicUtils;
 import java.security.SecureRandom;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class Sr25519UtilsTest {
+
+    private final byte[] hashedMessage = HashUtils.hashWithBlake2b("This is test message".getBytes());
+
+    @Test
+    void generateSignVerify() {
+        Schnorrkel.KeyPair keyPair = Sr25519Utils.generateKeyPair();
+
+        byte[] signed = Sr25519Utils.signMessage(keyPair.getPublicKey(), keyPair.getSecretKey(), hashedMessage);
+
+        VerifySignature signature = new VerifySignature(signed, hashedMessage, keyPair.getPublicKey(), Key.ED25519);
+        boolean verified = Sr25519Utils.verifySignature(signature);
+
+        assertTrue(verified);
+    }
 
     @Test
     void generateKeyFromSeedNoException(){
