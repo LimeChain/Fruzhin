00d07ab676ebcba56f5630043d5a81e510f1f483
Мurаd Hаmzа <murad.hamza@limechain.tech>
2023-10-18 15:56:05 +0300

    chore: use lombok instead of IDE generated hascode and equals

--- src/main/java/com/limechain/runtime/hostapi/dto/VerifySignature.java
+++ src/main/java/com/limechain/runtime/hostapi/dto/VerifySignature.java
@@ -1,13 +1,12 @@
 package com.limechain.runtime.hostapi.dto;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Arrays;
-import java.util.Objects;
-
 @Getter
 @Setter
+@EqualsAndHashCode
 public class VerifySignature {
 
     private byte[] signatureData;
@@ -23,22 +22,4 @@ public class VerifySignature {
         this.key = key;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        VerifySignature verifySig = (VerifySignature) o;
-        return Arrays.equals(signatureData, verifySig.signatureData) &&
-                Arrays.equals(messageData, verifySig.messageData) &&
-                Arrays.equals(publicKeyData, verifySig.publicKeyData) && key == verifySig.key;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(key);
-        result = 31 * result + Arrays.hashCode(signatureData);
-        result = 31 * result + Arrays.hashCode(messageData);
-        result = 31 * result + Arrays.hashCode(publicKeyData);
-        return result;
-    }
 }
