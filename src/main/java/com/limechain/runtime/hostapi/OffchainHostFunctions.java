c059a4b88333630f17c6787ddaedb5883544f383
Stoyan Stoyanov <stoyan.stoyanov@limechain.tech>
2023-10-19 12:42:09 +0300

    Implement some of the offchain functions

--- src/main/java/com/limechain/runtime/hostapi/OffchainHostFunctions.java
+++ src/main/java/com/limechain/runtime/hostapi/OffchainHostFunctions.java
@@ -1,10 +1,26 @@
 package com.limechain.runtime.hostapi;
 
-import lombok.experimental.UtilityClass;
+import com.limechain.config.HostConfig;
+import com.limechain.network.Network;
+import com.limechain.network.protocol.blockannounce.NodeRole;
+import com.limechain.rpc.server.AppBean;
+import io.emeraldpay.polkaj.scale.ScaleCodecWriter;
+import io.emeraldpay.polkaj.scaletypes.Result;
+import io.emeraldpay.polkaj.scaletypes.ResultWriter;
+import io.libp2p.core.PeerId;
+import io.libp2p.core.multiformats.Multiaddr;
+import lombok.AllArgsConstructor;
 import org.wasmer.ImportObject;
 import org.wasmer.Type;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -13,29 +29,39 @@ import java.util.List;
  * {<a href="https://spec.polkadot.network/chap-host-api#sect-offchain-api">Offchain API</a>}
  * {<a href="https://spec.polkadot.network/chap-host-api#sect-offchainindex-api">Offchain index API</a>}
  */
-@UtilityClass
+@AllArgsConstructor
 public class OffchainHostFunctions {
+    private final HostApi hostApi;
+    private final HostConfig config;
 
+    public OffchainHostFunctions() {
+        hostApi = HostApi.getInstance();
+        config = AppBean.getBean(HostConfig.class);
+    }
     public static List<ImportObject> getFunctions() {
-        return Arrays.asList(
-                HostApi.getImportObject("ext_offchain_is_validator_version_1", argv -> {
-                    return argv;
-                }, HostApi.EMPTY_LIST_OF_TYPES, Type.I32),
-                HostApi.getImportObject("ext_offchain_submit_transaction_version_1", argv -> {
-                    return argv;
-                }, List.of(Type.I64), Type.I64),
-                HostApi.getImportObject("ext_offchain_network_state_version_1", argv -> {
-                    return argv;
-                }, HostApi.EMPTY_LIST_OF_TYPES, Type.I64),
-                HostApi.getImportObject("ext_offchain_timestamp_version_1", argv -> {
-                    return argv;
-                }, HostApi.EMPTY_LIST_OF_TYPES, Type.I64),
-                HostApi.getImportObject("ext_offchain_sleep_until_version_1", argv -> {
+        return new OffchainHostFunctions().buildFunctions();
+    }
 
-                }, List.of(Type.I64)),
-                HostApi.getImportObject("ext_offchain_random_seed_version_1", argv -> {
-                    return argv;
-                }, HostApi.EMPTY_LIST_OF_TYPES, Type.I32),
+    public List<ImportObject> buildFunctions() {
+        return Arrays.asList(
+                HostApi.getImportObject("ext_offchain_is_validator_version_1", argv ->
+                        Collections.singletonList(extOffchainIsValidator()),
+                        HostApi.EMPTY_LIST_OF_TYPES, Type.I32),
+                HostApi.getImportObject("ext_offchain_submit_transaction_version_1", argv ->
+                        List.of(extOffchainSubmitTransaction(new RuntimePointerSize(argv.get(0))).pointerSize()),
+                                List.of(Type.I64), Type.I64),
+                HostApi.getImportObject("ext_offchain_network_state_version_1", argv ->
+                        Collections.singletonList(extOffchainNetworkState().pointerSize())
+                        , HostApi.EMPTY_LIST_OF_TYPES, Type.I64),
+                HostApi.getImportObject("ext_offchain_timestamp_version_1", argv ->
+                        Collections.singletonList(extOffchainTimestamp()),
+                        HostApi.EMPTY_LIST_OF_TYPES, Type.I64),
+                HostApi.getImportObject("ext_offchain_sleep_until_version_1", argv ->
+                        extOffchainSleepUntil(argv.get(0).longValue()),
+                        List.of(Type.I64)),
+                HostApi.getImportObject("ext_offchain_random_seed_version_1", argv ->
+                        Collections.singletonList(extOffchainRandomSeed()),
+                        HostApi.EMPTY_LIST_OF_TYPES, Type.I32),
                 HostApi.getImportObject("ext_offchain_local_storage_set_version_1", argv -> {
 
                 }, List.of(Type.I32, Type.I64, Type.I64)),
@@ -75,4 +101,106 @@ public class OffchainHostFunctions {
         );
     }
 
+    /**
+     * Check whether the local node is a potential validator. Even if this function returns 1,
+     * it does not mean that any keys are configured or that the validator is registered in the chain.
+     *
+     * @return an integer which is equal to 1 if the local node is a potential validator or an integer equal to 0 if
+     * it is not.
+     */
+    public int extOffchainIsValidator() {
+        return config.getNodeRole() == NodeRole.AUTHORING ? 1 : 0;
+    }
+
+    /**
+     * Given a SCALE encoded extrinsic, this function submits the extrinsic to the Host’s transaction pool,
+     * ready to be propagated to remote peers.
+     *
+     * @param extrinsicPointer a pointer-size to the byte array storing the encoded extrinsic.
+     * @return a  pointer-size to the SCALE encoded Result value. Neither on success nor failure is there any
+     * additional data provided. The cause of a failure is implementation specific.
+     */
+    public RuntimePointerSize extOffchainSubmitTransaction(RuntimePointerSize extrinsicPointer) {
+        byte[] extrinsic = hostApi.getDataFromMemory(extrinsicPointer);
+        // TODO: add to transaction pool,  when implemented, and set success to the result of that operation
+        boolean success = true;
+
+        return hostApi.writeDataToMemory(scaleEncodedEmptyResult(success));
+    }
+
+    private byte[] scaleEncodedEmptyResult(boolean success) {
+        return new byte[] { (success ? Result.ResultMode.OK : Result.ResultMode.ERR).getValue() };
+    }
+
+    /**
+     * Returns the SCALE encoded, opaque information about the local node’s network state.
+     *
+     * @return a pointer-size to the SCALE encoded Result value.
+     * On success - it contains the Opaque network state structure .
+     * On failure, an empty value is yielded where its cause is implementation specific.
+     *
+     * @see <a href=https://spec.polkadot.network/chap-host-api#defn-opaque-network-state>Opaque Network State</a>
+     */
+    public RuntimePointerSize extOffchainNetworkState() {
+        Network network = Network.getNetwork();
+        PeerId peerId = network.getHost().getPeerId();
+        List<Multiaddr> multiAddresses = network.getHost().listenAddresses();
+        return hostApi.writeDataToMemory(scaleEncodedOpaqueNetwork(peerId, multiAddresses));
+    }
+
+    private byte[] scaleEncodedOpaqueNetwork(PeerId peerId, List<Multiaddr> multiAddresses) {
+        try (ByteArrayOutputStream buf = new ByteArrayOutputStream();
+             ScaleCodecWriter writer = new ScaleCodecWriter(buf)) {
+            ByteBuffer data = ByteBuffer.wrap(peerId.getBytes());
+            multiAddresses.stream().map(Multiaddr::serialize).forEach(data::put);
+
+            Result<byte[], Exception> result = new Result<>(Result.ResultMode.OK, data.array(), null);
+
+            new ResultWriter<byte[], Exception>()
+                    .writeResult(writer, ScaleCodecWriter::writeByteArray, null, result);
+            return buf.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Returns the current timestamp.
+     *
+     * @return the current UNIX timestamp (in milliseconds).
+     */
+    public long extOffchainTimestamp() {
+        return Instant.now().toEpochMilli();
+    }
+
+    /**
+     * Pause the execution until the deadline is reached.
+     *
+     * @param deadline the UNIX timestamp in milliseconds
+     */
+    public void extOffchainSleepUntil(long deadline) {
+        long timeToSleep = extOffchainTimestamp() - deadline;
+        try {
+            if(timeToSleep > 0) {
+                Thread.sleep(timeToSleep);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Generates a random seed. This is a truly random non-deterministic seed generated by the host environment.
+     *
+     * @return a pointer to the buffer containing the 256-bit seed.
+     */
+    public int extOffchainRandomSeed() {
+        byte[] seed;
+        try {
+            seed = SecureRandom.getInstanceStrong().generateSeed(32);
+        } catch (NoSuchAlgorithmException e) {
+            seed = SecureRandom.getSeed(32);
+        }
+        return hostApi.writeDataToMemory(seed).pointer();
+    }
 }
---
+++ src/test/java/com/limechain/runtime/hostapi/OffchainHostFunctionsTest.java
@@ -0,0 +1,65 @@
+package com.limechain.runtime.hostapi;
+
+import com.limechain.config.HostConfig;
+import com.limechain.network.protocol.blockannounce.NodeRole;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class OffchainHostFunctionsTest {
+    @InjectMocks
+    private OffchainHostFunctions offchainHostFunctions;
+
+    @Mock
+    private HostApi hostApi;
+    @Mock
+    private HostConfig config;
+
+    @Test
+    void extOffchainIsValidatorWhenNodeRoleIsAuthoringShouldReturnOne() {
+        when(config.getNodeRole()).thenReturn(NodeRole.AUTHORING);
+
+        int result = offchainHostFunctions.extOffchainIsValidator();
+
+        assertEquals(1, result);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = NodeRole.class, names = { "AUTHORING" }, mode = EnumSource.Mode.EXCLUDE)
+    void extOffchainIsValidatorWhenNodeRoleIsNotAuthoringShouldReturnZero(NodeRole nodeRole) {
+        when(config.getNodeRole()).thenReturn(nodeRole);
+
+        int result = offchainHostFunctions.extOffchainIsValidator();
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    void extOffchainTimestampShouldReturnCurrentTimeFromInstant() {
+        Instant instant = mock(Instant.class);
+        long time = 123L;
+
+        try(MockedStatic<Instant> mockedStatic = mockStatic(Instant.class)) {
+            mockedStatic.when(Instant::now).thenReturn(instant);
+            when(instant.toEpochMilli()).thenReturn(time);
+
+            long result = offchainHostFunctions.extOffchainTimestamp();
+
+            assertEquals(time, result);
+        }
+    }
+}
