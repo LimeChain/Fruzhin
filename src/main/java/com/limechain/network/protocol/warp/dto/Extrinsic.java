00d07ab2b59a5ddd4a8410f8559d5291ac9bcdf9
Мurаd Hаmzа <murad.hamza@limechain.tech>
2023-11-16 08:07:38 +0200

    chore: make extrinsics final

--- src/main/java/com/limechain/network/protocol/warp/dto/Extrinsic.java
+++ src/main/java/com/limechain/network/protocol/warp/dto/Extrinsic.java
@@ -2,7 +2,6 @@ package com.limechain.network.protocol.warp.dto;
 
 import com.limechain.utils.HashUtils;
 import io.emeraldpay.polkaj.scale.ScaleCodecWriter;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.apache.tomcat.util.buf.HexUtils;
 
@@ -10,9 +9,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 @Data
-@AllArgsConstructor
 public class Extrinsic {
-    private byte[] extrinsic;
+
+    private final byte[] extrinsic;
 
     @Override
     public String toString(){
