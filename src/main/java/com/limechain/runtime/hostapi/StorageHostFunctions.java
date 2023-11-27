246a4caadf91243a4aed1e267298acc9f7d16ac9
bokoto000 <bobivelkdev@gmail.com>
2023-11-27 09:28:52 +0200

    Child storage api (#239)
    * Child storage api
    
    * Fix build
    
    * Disable integration tests
    
    * Refactor
    
    * Fix solar

--- src/main/java/com/limechain/runtime/hostapi/StorageHostFunctions.java
+++ src/main/java/com/limechain/runtime/hostapi/StorageHostFunctions.java
@@ -318,7 +318,7 @@ public class StorageHostFunctions {
         repository.commitTransaction();
     }
 
-    private byte[] scaleEncodedOption(@Nullable int data) {
+    public static byte[] scaleEncodedOption(@Nullable int data) {
         ByteArrayOutputStream buf = new ByteArrayOutputStream();
         try (ScaleCodecWriter writer = new ScaleCodecWriter(buf)) {
             writer.writeOptional(ScaleCodecWriter::writeUint32, data);
@@ -328,7 +328,7 @@ public class StorageHostFunctions {
         return buf.toByteArray();
     }
 
-    private byte[] scaleEncodedOption(@Nullable byte[] data) {
+    public static byte[] scaleEncodedOption(@Nullable byte[] data) {
         ByteArrayOutputStream buf = new ByteArrayOutputStream();
         try (ScaleCodecWriter writer = new ScaleCodecWriter(buf)) {
             writer.writeOptional(ScaleCodecWriter::writeByteArray, data);
