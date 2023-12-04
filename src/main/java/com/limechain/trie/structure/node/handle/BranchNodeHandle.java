00d07ab80ea39ad088105f4de627dd6ab83fc115
Мurаd Hаmzа <murad.hamza@limechain.tech>
2023-12-04 09:20:45 +0200

    build: fix

--- src/main/java/com/limechain/trie/structure/node/handle/BranchNodeHandle.java
+++ src/main/java/com/limechain/trie/structure/node/handle/BranchNodeHandle.java
@@ -1,9 +1,10 @@
 package com.limechain.trie.structure.node.handle;
 
 import com.limechain.trie.structure.TrieStructure;
+import com.limechain.trie.structure.node.TrieNodeIndex;
 
 public final class BranchNodeHandle<T> extends NodeHandle<T> {
-    public BranchNodeHandle(TrieStructure<T> trieStructure, int nodeIndex) {
+    public BranchNodeHandle(TrieStructure<T> trieStructure, TrieNodeIndex nodeIndex) {
         super(trieStructure, nodeIndex);
     }
 }
