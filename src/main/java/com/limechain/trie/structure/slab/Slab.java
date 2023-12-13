2bed85e4fba2ac3ed55444dd84a7f732b5fec7e2
bokoto000 <bobivelkdev@gmail.com>
2023-12-13 11:21:36 +0200

    Add docs and tests

--- src/main/java/com/limechain/trie/structure/slab/Slab.java
+++ src/main/java/com/limechain/trie/structure/slab/Slab.java
@@ -5,81 +5,160 @@ import org.javatuples.Pair;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Queue;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+/**
+ * Represents a slab data structure for efficient management of a fixed type of objects.
+ * It provides methods for adding, removing, and accessing elements, along with utilities
+ * for managing the slab's capacity and contents.
+ *
+ * @param <T> the type of elements held in this slab
+ */
 public class Slab<T> implements Iterable<Pair<Integer, T>> {
     private ArrayList<T> storage;
     private int newIndex;
     private Queue<Integer> freeIndices;
+    private int size;
 
+    /**
+     * Constructs an empty Slab with default initial capacity of 0.
+     */
     public Slab() {
         initialize(0);
     }
 
+    /**
+     * Constructs a Slab with the specified initial capacity.
+     *
+     * @param initialCapacity the initial capacity of the slab
+     */
     public Slab(int initialCapacity) {
         initialize(initialCapacity);
     }
 
+    /**
+     * Initializes the internal storage structures of the slab.
+     *
+     * @param initialCapacity the initial capacity of the slab
+     */
     private void initialize(int initialCapacity) {
         this.storage = new ArrayList<>(initialCapacity);
         this.newIndex = 0;
-        this.freeIndices = new LinkedList<>();
+        this.size = 0;
+        this.freeIndices = new PriorityQueue<>();
     }
 
+    /**
+     * Adds an element to the slab and returns its index.
+     *
+     * @param element the element to be added, must not be null
+     * @return the index at which the element is stored
+     * @throws NullPointerException if the element is null
+     */
     public int add(@NonNull T element) {
-        int index = freeIndices.isEmpty() ? newIndex++ : freeIndices.poll();
-        storage.add(index, element);
+        int index;
+        if(freeIndices.isEmpty()){
+            index = newIndex;
+            storage.add(element);
+            newIndex++;
+        } else{
+            index = freeIndices.poll();
+            storage.set(index, element);
+        }
+        size++;
         return index;
     }
 
+    /**
+     * Removes the element at the specified index from the slab.
+     *
+     * @param index the index of the element to be removed
+     * @return the element previously at the specified index
+     */
     public T remove(int index) {
         T value = storage.get(index);
         storage.set(index, null);
         freeIndices.add(index);
+        size--;
         return value;
     }
 
+    /**
+     * Retrieves the element at the specified index.
+     *
+     * @param index the index of the element to return
+     * @return the element at the specified index, or null if no element is present
+     */
     public @Nullable T get(int index) {
         return storage.get(index);
     }
 
+    /**
+     * Removes all elements from the slab.
+     */
     public void clear() {
         initialize(0);
     }
 
+    /**
+     * Returns a list of all non-null elements in the slab along with their indices.
+     *
+     * @return a list of pairs, where each pair contains an index and a non-null element
+     */
     public List<Pair<Integer, T>> getAllEntries() {
         return streamAllEntries().collect(Collectors.toList());
     }
 
+    /**
+     * Removes all elements from the slab and returns them as a list.
+     *
+     * @return a list of pairs, where each pair contains an index and an element,
+     * representing all elements removed from the slab
+     */
     public List<Pair<Integer, T>> drain() {
         var result = getAllEntries();
         clear();
         return result;
     }
 
+    /**
+     * Checks if the slab is empty.
+     *
+     * @return true if the slab contains no elements
+     */
     public boolean isEmpty() {
         return storage.isEmpty();
     }
 
+    /**
+     * Returns the number of elements in the slab.
+     *
+     * @return the number of elements in the slab
+     */
     public int size() {
-        return storage.size();
+        return size;
     }
 
+    /**
+     * Provides a stream of all non-null elements in the slab along with their indices.
+     *
+     * @return a stream of pairs, where each pair contains an index and a non-null element
+     */
     private Stream<Pair<Integer, T>> streamAllEntries() {
         return IntStream.range(0, storage.size())
-            .mapToObj(i -> new Pair<>(i, storage.get(i)))
-            .filter(p -> Objects.nonNull(p.getValue1()));
+                .mapToObj(i -> new Pair<>(i, storage.get(i)))
+                .filter(p -> Objects.nonNull(p.getValue1()));
     }
 
+    /**
+     * Returns an iterator over elements of type {@code Pair<Integer, T>}.
+     * Each pair contains an index and a non-null element from the slab.
+     *
+     * @return an Iterator.
+     */
     @NotNull
     @Override
     public Iterator<Pair<Integer, T>> iterator() {
--- src/test/java/com/limechain/trie/structure/slab/SlabTest.java
+++ src/test/java/com/limechain/trie/structure/slab/SlabTest.java
@@ -1,25 +1,186 @@
 package com.limechain.trie.structure.slab;
 
+import org.javatuples.Pair;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SlabTest {
     @Test
-    void test_Simple_Retrieval_Works() {
+    void addAndGetTest() {
         Slab<String> slab = new Slab<>();
-        String s = "alo";
-        int i = slab.add(s);
-        assertEquals(s, slab.get(i));
+        int index = slab.add("test");
+        assertEquals(0, index);
+        assertEquals("test", slab.get(index));
     }
 
     @Test
-    void test_Simple_Removal_Works() {
+    void removeTest() {
         Slab<String> slab = new Slab<>();
-        String s = "alo";
-        int i = slab.add(s);
-        assertEquals(s, slab.get(i));
-        slab.remove(i);
-        assertNull(slab.get(i));
+        int index = slab.add("test");
+        assertEquals("test", slab.remove(index));
+        assertNull(slab.get(index));
+    }
+
+    @Test
+    void clearTest() {
+        Slab<String> slab = new Slab<>();
+        slab.add("test1");
+        slab.add("test2");
+        slab.clear();
+        assertTrue(slab.isEmpty());
+    }
+
+    @Test
+    void isEmptyTest() {
+        Slab<String> slab = new Slab<>();
+        assertTrue(slab.isEmpty());
+        slab.add("test");
+        assertFalse(slab.isEmpty());
+    }
+
+    @Test
+    void sizeTest() {
+        Slab<String> slab = new Slab<>();
+        slab.add("test1");
+        slab.add("test2");
+        assertEquals(2, slab.size());
+    }
+
+    @Test
+    void drainTest() {
+        Slab<String> slab = new Slab<>();
+        slab.add("test1");
+        slab.add("test2");
+        List<Pair<Integer, String>> drained = slab.drain();
+        assertEquals(2, drained.size());
+        assertTrue(slab.isEmpty());
+    }
+
+    @Test
+    void iteratorTest() {
+        Slab<String> slab = new Slab<>();
+        slab.add("test1");
+        slab.add("test2");
+        Iterator<org.javatuples.Pair<Integer, String>> it = slab.iterator();
+        assertTrue(it.hasNext());
+        assertNotNull(it.next());
+        assertTrue(it.hasNext());
+        assertNotNull(it.next());
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    void initialCapacityIncreaseTest() {
+        int initialCapacity = 10;
+        Slab<String> slab = new Slab<>(initialCapacity);
+
+        for (int i = 0; i < initialCapacity; i++) {
+            slab.add("test " + i);
+        }
+
+        assertEquals(initialCapacity, slab.size());
+
+        slab.add("Extra Element");
+        assertEquals(initialCapacity + 1, slab.size());
+    }
+
+    @Test
+    void removeNonExistentElementThrowsErrorTest() {
+        Slab<String> slab = new Slab<>();
+
+        slab.add("test1");
+        slab.add("test2");
+
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            slab.remove(5);
+        });
+
+        assertEquals(2, slab.size());
+    }
+
+    @Test
+    void removeNegativeIndexThrowsExceptionTest() {
+        Slab<String> slab = new Slab<>();
+
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            slab.remove(-1);
+        });
+    }
+
+    @Test
+    void reuseIndexTest() {
+        Slab<String> slab = new Slab<>();
+
+        int initialIndex = slab.add("test1");
+        slab.remove(initialIndex);
+
+        int reusedIndex = slab.add("test2");
+        assertEquals(initialIndex, reusedIndex);
+
+        assertEquals("test2", slab.get(reusedIndex));
+    }
+
+    @Test
+    void emptySlabIteratorIsEmptyTest() {
+        Slab<String> slab = new Slab<>();
+
+        Iterator<Pair<Integer, String>> iterator = slab.iterator();
+
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    void testIteratorOverElements() {
+        Slab<String> slab = new Slab<>();
+        List<String> addedElements = new ArrayList<>();
+
+        addedElements.add("test1");
+        slab.add("test1");
+        addedElements.add("test2");
+        slab.add("test2");
+
+        slab.remove(0);
+        addedElements.remove(0);
+
+        List<String> iteratedElements = new ArrayList<>();
+        for (Pair<Integer, String> pair : slab) {
+            iteratedElements.add(pair.getValue1());
+        }
+
+        assertEquals(addedElements, iteratedElements);
+    }
+
+    @Test
+    void testConsecutiveAddsAndRemoves() {
+        Slab<String> slab = new Slab<>();
+        Set<Integer> addedIndices = new HashSet<>();
+
+        for (int i = 0; i < 5; i++) {
+            int index = slab.add("test" + i);
+            addedIndices.add(index);
+        }
+
+        slab.remove(1);
+        slab.remove(3);
+        addedIndices.remove(1);
+        addedIndices.remove(3);
+
+        for (int i = 5; i < 7; i++) {
+            int index = slab.add("test" + i);
+            assertTrue(addedIndices.add(index));
+        }
+
+        assertEquals(addedIndices.size(), slab.size());
+
+        for (Integer index : addedIndices) {
+            assertNotNull(slab.get(index));
+        }
     }
 }
