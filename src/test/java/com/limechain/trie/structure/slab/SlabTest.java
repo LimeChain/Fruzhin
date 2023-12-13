8a7a5a57ca2abee8433f84c6349fbb64d2a9a513
bokoto000 <bobivelkdev@gmail.com>
2023-12-13 13:29:12 +0200

    Refactor imports

--- src/test/java/com/limechain/trie/structure/slab/SlabTest.java
+++ src/test/java/com/limechain/trie/structure/slab/SlabTest.java
@@ -9,7 +9,12 @@ import java.util.Iterator;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SlabTest {
     @Test
