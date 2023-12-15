60ecb92c2383e703729c927ec8bbd2330fb5f457
bokoto000 <bobivelkdev@gmail.com>
2023-12-15 10:34:44 +0200

    Fix imports

--- src/test/java/com/limechain/runtime/hostapi/OffchainHttpRequestsTest.java
+++ src/test/java/com/limechain/runtime/hostapi/OffchainHttpRequestsTest.java
@@ -11,7 +11,10 @@ import java.net.HttpURLConnection;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class OffchainHttpRequestsTest {
     @Mock
