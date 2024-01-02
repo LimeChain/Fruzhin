49d6262fa99c5f1fe7cbf6650a4b8d4d770d7f93
Stoyan Stoyanov <stoyan.stoyanov@limechain.tech>
2024-01-02 16:43:27 +0200

    Remove duplicate exception

--- src/main/java/com/limechain/runtime/hostapi/dto/ScaleEncodingException.java
+++
@@ -1,7 +0,0 @@
-package com.limechain.runtime.hostapi.dto;
-
-public class ScaleEncodingException extends RuntimeException {
-    public ScaleEncodingException(Throwable cause) {
-        super("Scale encoding failed!", cause);
-    }
-}
