be5b215f1f947c7633c9d810e43639fb4fa57a37
bokoto000 <bobivelkdev@gmail.com>
2024-01-04 11:08:50 +0200

    Refactor

--- src/test/java/com/limechain/trie/structure/decoded/node/DecodedNodeTest.java
+++ src/test/java/com/limechain/trie/structure/decoded/node/DecodedNodeTest.java
@@ -10,7 +10,9 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class DecodedNodeTest {
 
