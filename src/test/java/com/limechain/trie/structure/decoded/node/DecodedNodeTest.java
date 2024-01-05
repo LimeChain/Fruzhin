ef8762ad16fca0aff044ca242b3162467bde1ad4
bokoto000 <bobivelkdev@gmail.com>
2024-01-05 17:03:46 +0200

    Fix

--- src/test/java/com/limechain/trie/structure/decoded/node/DecodedNodeTest.java
+++ src/test/java/com/limechain/trie/structure/decoded/node/DecodedNodeTest.java
@@ -29,15 +29,15 @@ class DecodedNodeTest {
         byte[] expectedChild1 = new byte[]{(byte) 129, (byte) 254, 111, 21, 39, (byte) 188, (byte) 215, 18, (byte) 139,
                 76, (byte) 128, (byte) 157, 108, 33, (byte) 139, (byte) 232, 34, 73, 0, 21, (byte) 202, 54, 18, 71,
                 (byte) 145, 117, 47, (byte) 222, (byte) 189, 93, 119, 68};
-        byte[] expectedChild2 = new byte[]{108,(byte) 211, 105, 98, 122,(byte) 206, (byte) 246, 73, 77, (byte) 237, 51,
-                77, 26, (byte) 166, 1, 52, (byte) 179, (byte)173, 43, 89, (byte) 219, 104, (byte) 196, (byte) 190,
+        byte[] expectedChild2 = new byte[]{108, (byte) 211, 105, 98, 122, (byte) 206, (byte) 246, 73, 77, (byte) 237, 51,
+                77, 26, (byte) 166, 1, 52, (byte) 179, (byte) 173, 43, 89, (byte) 219, 104, (byte) 196, (byte) 190,
                 (byte) 208, (byte) 128, (byte) 135, (byte) 177, 13, (byte) 185, 111, (byte) 175};
         List<Byte> boxedExpectedEncoded = Arrays.asList(ArrayUtils.toObject(expectedEncoded));
         List<Byte> boxedExpectedChild1 = Arrays.asList(ArrayUtils.toObject(expectedChild1));
         List<Byte> boxedExpectedChild2 = Arrays.asList(ArrayUtils.toObject(expectedChild2));
         StorageValue expectedStorageValue = new StorageValue(new byte[]{}, false);
         int expectedNotNullChildren = 2;
-Nibbles expectedPartialKey = Nibbles.fromHexString("63");
+        Nibbles expectedPartialKey = Nibbles.fromHexString("63");
 
         DecodedNode<Nibbles, List<Byte>> decoded = DecodedNode.decode(expectedEncoded);
         assertNotNull(decoded);
@@ -45,7 +45,7 @@ Nibbles expectedPartialKey = Nibbles.fromHexString("63");
         assertArrayEquals(boxedExpectedEncoded.toArray(), decoded.encode()
                 .flatMap(Collection::stream).collect(Collectors.toList()).toArray());
 
-        assertEquals(expectedNibbles, decoded.getPartialKey());
+        assertEquals(expectedPartialKey, decoded.getPartialKey());
 
         StorageValue storageValue = decoded.getStorageValue();
         assertEquals(expectedStorageValue, storageValue);
