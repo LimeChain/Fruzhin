79be89a92ddb395a8427d51ee23e6c8467bed265
David Petrov <david.petrov@limechain.tech>
2024-01-12 18:12:38 +0200

    chore(sonarlint): remove unused import

--- src/main/java/com/limechain/trie/structure/slab/Slab.java
+++ src/main/java/com/limechain/trie/structure/slab/Slab.java
@@ -10,7 +10,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Queue;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
