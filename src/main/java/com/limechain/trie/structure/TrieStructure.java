70843581c863b75c4b1daf1e679891ef8680e6dd
David Petrov <david.petrov@limechain.tech>
2024-01-15 11:37:41 +0200

    refactor!: remove `equals` overriding
    https://github.com/LimeChain/Fruzhin/pull/265#discussion_r1452108376

--- src/main/java/com/limechain/trie/structure/TrieStructure.java
+++ src/main/java/com/limechain/trie/structure/TrieStructure.java
@@ -455,16 +455,4 @@ public class TrieStructure<T> {
             }
         }
     }
-
-    // TODO: Figure out how to sensibly deprecate this or whether it should be equivalent to structurallyEquals
-    /**
-     * @deprecated
-     * This method does nothing more than {@link Object#equals}, i.e. referential equality.
-     * <p> Use {@link TrieStructure#structurallyEquals(TrieStructure other)} instead.
-     */
-    @Override
-    @Deprecated
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
 }
