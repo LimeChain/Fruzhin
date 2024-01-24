78a2dbf45753edb939e7cf09b87d0bd4258d180f
Stoyan Stoyanov <stoyan.stoyanov@limechain.tech>
2024-01-24 13:07:33 +0200

    Fix InsertTrieNode arrays equality

--- src/main/java/com/limechain/trie/structure/node/InsertTrieNode.java
+++ src/main/java/com/limechain/trie/structure/node/InsertTrieNode.java
@@ -2,7 +2,9 @@ package com.limechain.trie.structure.node;
 
 import com.limechain.trie.structure.nibble.Nibble;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents a node in a trie structure for insertion into a database.
@@ -14,8 +16,36 @@ import java.util.List;
  * @param childrenMerkleValues A list of merkle values of the children of this trie node.
  *                             Each entry in the list is a byte array representing the merkle value of a child node.
  * @param partialKeyNibbles    A list of nibbles representing the partial key associated with this trie node.
- * @param isReferenceValue       Notes if the value stored is a reference to anothed node
+ * @param isReferenceValue     Notes if the value stored is a reference to anothed node
  */
 public record InsertTrieNode(byte[] storageValue, byte[] merkleValue, List<byte[]> childrenMerkleValues,
                              List<Nibble> partialKeyNibbles, boolean isReferenceValue) {
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (InsertTrieNode) obj;
+        return Arrays.equals(this.storageValue, that.storageValue) &&
+                Arrays.equals(this.merkleValue, that.merkleValue) &&
+                Objects.equals(this.childrenMerkleValues, that.childrenMerkleValues) &&
+                Objects.equals(this.partialKeyNibbles, that.partialKeyNibbles) &&
+                this.isReferenceValue == that.isReferenceValue;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Arrays.hashCode(storageValue), Arrays.hashCode(merkleValue), childrenMerkleValues,
+                partialKeyNibbles, isReferenceValue);
+    }
+
+    @Override
+    public String toString() {
+        return "InsertTrieNode[" +
+                "storageValue=" + Arrays.toString(storageValue) + ", " +
+                "merkleValue=" + Arrays.toString(merkleValue) + ", " +
+                "childrenMerkleValues=" + childrenMerkleValues + ", " +
+                "partialKeyNibbles=" + partialKeyNibbles + ", " +
+                "isReferenceValue=" + isReferenceValue + ']';
+    }
 }
