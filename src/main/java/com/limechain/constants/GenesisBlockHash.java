00d07ab32da45af84b4d207fe1ac8546f0d23f55
Мurаd Hаmzа <murad.hamza@limechain.tech>
2024-01-25 16:16:34 +0200

    chore: add temporary realistic genesis values

--- src/main/java/com/limechain/constants/GenesisBlockHash.java
+++ src/main/java/com/limechain/constants/GenesisBlockHash.java
@@ -1,8 +1,10 @@
 package com.limechain.constants;
 
 import com.limechain.chain.ChainService;
+import com.limechain.network.Network;
 import com.limechain.network.protocol.warp.dto.BlockHeader;
 import com.limechain.network.protocol.warp.dto.HeaderDigest;
+import com.limechain.rpc.server.AppBean;
 import com.limechain.trie.structure.NodeHandle;
 import com.limechain.trie.structure.TrieStructure;
 import com.limechain.trie.structure.database.InsertTrieBuilder;
@@ -28,7 +30,24 @@ public class GenesisBlockHash {
         this.chainService = chainService;
         this.genesisTrie = buildGenesisTrie();
         this.genesisBlockHeader = buildGenesisBlock(getMerkleValue(this.genesisTrie));
-        this.genesisHash = this.genesisBlockHeader.getHash();
+//        this.genesisHash = this.genesisBlockHeader.getHash(); //TODO: Uncomment when fixed
+
+        //TODO: only temporary solution
+        Hash256 POLKADOT = Hash256.from("91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3");
+        Hash256 WESTEND = Hash256.from("e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e");
+        Hash256 KUSAMA = Hash256.from("b0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe");
+        Hash256 LOCAL = Hash256.from("f825d3c387965817822a437528e6f4b659480b54d072c8d99b8af8451185c58f");
+
+        Network network = AppBean.getBean(Network.class);
+
+        switch (network.getChain()) {
+            case POLKADOT -> genesisHash = POLKADOT;
+            case KUSAMA -> genesisHash = KUSAMA;
+            case WESTEND -> genesisHash = WESTEND;
+            case LOCAL -> genesisHash = LOCAL;
+            default -> throw new IllegalStateException("Unexpected value: " + network.getChain());
+        }
+        //TODO!: only temporary solution
     }
 
     public BlockHeader buildGenesisBlock(byte[] merkleValue) {
