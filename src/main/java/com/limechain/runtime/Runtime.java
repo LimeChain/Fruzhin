724d3f3508dc1ab17a63a86335d34988b703eddd
David Petrov <david.petrov@limechain.tech>
2024-02-20 13:59:54 +0200

    init: initial commit

--- src/main/java/com/limechain/runtime/Runtime.java
+++ src/main/java/com/limechain/runtime/Runtime.java
@@ -6,6 +6,7 @@ import com.limechain.runtime.hostapi.dto.RuntimePointerSize;
 import com.limechain.runtime.version.RuntimeVersion;
 import lombok.Getter;
 import lombok.extern.java.Log;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.wasmer.Instance;
 import org.wasmer.Memory;
@@ -30,13 +31,35 @@ public class Runtime {
         hostApi.updateAllocator();
     }
 
-    // TODO: Add adequate parameters to the runtime call
+    /**
+     * Calls an exported runtime function with no parameters.
+     * @param functionName the name of the function
+     * @return the SCALE encoded response
+     */
     @Nullable
     public byte[] call(String functionName) {
+        return callInner(functionName, new RuntimePointerSize(0, 0));
+    }
+
+    /**
+     * Calls an exported runtime function with parameters.
+     * @param functionName the name of the function
+     * @param parameter the SCALE encoded tuple of parameters
+     * @return the SCALE encoded response
+     */
+    public byte[] call(String functionName, @NotNull byte[] parameter) {
+        return callInner(functionName, allocateCallParameter(parameter));
+    }
+
+    private RuntimePointerSize allocateCallParameter(@NotNull byte[] parameter) {
+        // TODO: We need an allocator here to write the parameter in the runtime memory
+        return new RuntimePointerSize(0, 0);
+    }
+
+    private byte[] callInner(String functionName, RuntimePointerSize parameterPtrSize) {
         log.log(Level.INFO, "Making a runtime call: " + functionName);
-        Object[] response = instance.exports
-            .getFunction(functionName)
-            .apply(0, 0);
+        Object[] response = instance.exports.getFunction(functionName)
+            .apply(parameterPtrSize.pointer(), parameterPtrSize.size());
 
         if (response == null) {
             return null;
@@ -49,8 +72,8 @@ public class Runtime {
     /**
      * This setter exists to be used only in the RuntimeBuilder, since the RuntimeVersion is essentially an attribute
      * of the Runtime, thus modeled as its field. If we can't find it in a custom section directly from the binary
-     * though, we'll still need an instance of `Runtime` in order to obtain the version, and set it afterward via
-     * this setter.
+     * though, we'll still need an instance of `Runtime` in order to obtain the version by calling `Core_version`,
+     * and set it afterward via this setter.
      */
     void setVersion(RuntimeVersion runtimeVersion) {
         this.version = runtimeVersion;
---
+++ src/main/java/com/limechain/sync/fullsync/inherents/InherentData.java
@@ -0,0 +1,39 @@
+package com.limechain.sync.fullsync.inherents;
+
+import org.javatuples.Pair;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+/**
+ * Represents the totality of inherentes included in each block.
+ * @param timestamp Number of milliseconds since the UNIX epoch when the block is generated, ignoring leap seconds.
+ *                  Its identifier passed to the runtime is: `timstap0`.
+ * @see <a href="https://spec.polkadot.network/chap-state#tabl-inherent-data">the spec</a>
+ */
+public record InherentData(
+    long timestamp
+    // TODO: parachain-related inherents are missing
+) {
+
+    /**
+     * Turns this list of inherents into a list that can be passed as parameter to the runtime.
+     */
+    public List<Pair<byte[], byte[]>> asRawList() {
+        return List.of(
+            new Pair<>(
+                "timstap0".getBytes(StandardCharsets.US_ASCII),
+                longToBytesLittleEndian(timestamp)
+            )
+        );
+    }
+
+    private static byte[] longToBytesLittleEndian(long value) {
+        byte[] bytes = new byte[8];
+        for (int i = 0; i < 8; i++) {
+            bytes[i] = (byte) (value & 0xFF);
+            value >>= 8;
+        }
+        return bytes;
+    }
+}
