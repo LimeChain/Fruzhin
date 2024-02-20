00d07ab6ae5edb497d5eedea96b5056ebc318974
Мurаd Hаmzа <murad.hamza@limechain.tech>
2024-02-20 16:36:42 +0200

    test: run with mockito

--- src/test/java/com/limechain/rpc/methods/offchain/OffchainRPCImplTest.java
+++ src/test/java/com/limechain/rpc/methods/offchain/OffchainRPCImplTest.java
@@ -2,10 +2,11 @@ package com.limechain.rpc.methods.offchain;
 
 import com.limechain.storage.KVRepository;
 import com.limechain.storage.offchain.StorageKind;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
@@ -13,17 +14,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class OffchainRPCImplTest {
     @Mock
     private KVRepository<String, Object> dbMock;
-
+    @InjectMocks
     private OffchainRPCImpl offchainRPC;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        offchainRPC = new OffchainRPCImpl(dbMock);
-    }
 
     @Test
     void testOffchainLocalStorageSetPersistent() {
@@ -44,7 +40,7 @@ class OffchainRPCImplTest {
         String value = "0xcdef";
 
         String keyStr = "test";
-        byte[] valueBytes = new byte[]{(byte)0xCD, (byte)0xEF};
+        byte[] valueBytes = new byte[]{(byte) 0xCD, (byte) 0xEF};
 
         offchainRPC.offchainLocalStorageSet(StorageKind.LOCAL, key, value);
 
@@ -71,7 +67,7 @@ class OffchainRPCImplTest {
         String key = "0x74657374";
 
         String keyStr = "test";
-        byte[] returnValue = new byte[]{(byte)0xCD, (byte)0xEF};
+        byte[] returnValue = new byte[]{(byte) 0xCD, (byte) 0xEF};
         String expectedValue = "0xcdef";
 
         doReturn(Optional.of(returnValue)).when(dbMock).find("offchain_local_" + keyStr);
