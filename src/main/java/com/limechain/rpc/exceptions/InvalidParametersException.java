00d07aba4545a809611909750017f82c89dd7995
Мurаd Hаmzа <murad.hamza@limechain.tech>
2024-02-20 17:11:25 +0200

    chore: reformat exception

--- src/main/java/com/limechain/rpc/exceptions/InvalidParametersException.java
+++ src/main/java/com/limechain/rpc/exceptions/InvalidParametersException.java
@@ -1,6 +1,6 @@
 package com.limechain.rpc.exceptions;
 
-public class InvalidParametersException extends RuntimeException{
+public class InvalidParametersException extends RuntimeException {
     public InvalidParametersException(String message) {
         super(message);
     }
