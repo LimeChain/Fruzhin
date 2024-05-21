f3261af5b7329ed5684b19d7035175ea7b1733c9
Yordan Atanasov <yordan.atanasov@limechain.tech>
2024-05-21 15:44:27 +0300

    Fix script param check logic.

--- local_dev.sh
+++ local_dev.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ "$2" = "true" ] && [ $# != 5 ]; then
+if [ "$2" = "true" ]; then
+  if [ $# != 5 ]; then
     echo "Usage: $(basename $0) PORT(INT) START_FRUZHIN(BOOL) NODE_MODE(STRING) SYNC_MODE(STRING) DB_RECREATE(BOOL)"
     exit 1
-elif [ "$2" = "false" ] && [ $# != 2 ]; then
+  fi
+elif [ $# != 2 ]; then
     echo "Usage: $(basename $0) PORT(INT) START_FRUZHIN(BOOL)"
     exit 1
 fi
@@ -22,8 +24,7 @@ final_json=$(echo "$output_json" | jq --argjson bootNodes "$boot_nodes" '.bootNo
 
 echo "$final_json" > "./genesis/westend-local.json"
 
-if $2
-then
+if [ "$2" = "true" ]; then
     ./gradlew clean build
      java -jar build/libs/Fruzhin-0.1.0.jar -n local --node-mode $3 --sync-mode $4 --db-recreate $5
 fi
