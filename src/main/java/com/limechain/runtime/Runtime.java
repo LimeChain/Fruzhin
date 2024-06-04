a395403f58fe3a9df1bbbde5610af02fc633c973
David Petrov <david.petrov@limechain.tech>
2024-06-04 15:08:22 +0300

    chore: move big TODO
    From a comment into TODO.md

--- src/main/java/com/limechain/runtime/Runtime.java
+++ src/main/java/com/limechain/runtime/Runtime.java
@@ -4,6 +4,8 @@ import com.limechain.runtime.hostapi.dto.RuntimePointerSize;
 import com.limechain.runtime.version.RuntimeVersion;
 import com.limechain.runtime.version.scale.RuntimeVersionReader;
 import com.limechain.utils.scale.ScaleUtils;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -13,31 +15,12 @@ import org.wasmer.Module;
 import java.util.logging.Level;
 
 @Log
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
 public class Runtime {
     // TODO: Figure out whether we'll actually need this field
     Module module; // used to open/close all of its own instances
     Context context;
     Instance instance;
-
-    // TODO: Reconsider whether `Context` should be a constructor argument or (dependency injected)
-    //                       OR an argument of `Runtime::call()` (dependency parameterized)
-    //  The former necessitates the explicit passing of a context for every build of the runtime
-    //      (we can't build the underlying module only - we always instantiate and tie the instance to the context)
-    //    - In more lightweight cases (i.e. only build to fetch the version), we'll have to explicitly provide a "partial" context;
-    //      "partial" meaning with some of the services inside being null (or partially implemented)
-    //      since we know they are not going to be used
-    //  The latter necessitates the explicit re-instantiating of the underlying `org.wasmer.Instance`
-    //      for each RuntimeAPI call (they do that in Gossamer),
-    //      but allows for easy caching of the compiled wasm module (thus we can pass that around, e.g. in the BlockState)
-    //      and instantiate with a context only when invocation is necessary.
-    //      This might prove more fitting, for example,
-    //      when executing consecutive blocks (between which there's no runtime updates)
-    //      without rebuilding the module and by only changing the context...
-    Runtime(Module module, Context context, Instance instance) {
-        this.module = module;
-        this.context = context;
-        this.instance = instance;
-    }
 
     /**
      * Calls an exported runtime function with no parameters.
