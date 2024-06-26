8a978a8d547585d388d8db62f7717d5b66eb9b5a
Yordan Atanasov <yordan.atanasov@limechain.tech>
2024-06-26 11:12:30 +0300

    fix: Override equals, hashcode and tostring.

--- src/main/java/com/limechain/trie/cache/node/PendingInsertUpdate.java
+++ src/main/java/com/limechain/trie/cache/node/PendingInsertUpdate.java
@@ -2,8 +2,34 @@ package com.limechain.trie.cache.node;
 
 import com.limechain.trie.structure.nibble.Nibbles;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public record PendingInsertUpdate(byte[] newMerkleValue, Nibbles partialKey,
                                   List<byte[]> childrenMerkleValues) implements PendingTrieNodeChange {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PendingInsertUpdate that = (PendingInsertUpdate) o;
+        return Objects.equals(partialKey, that.partialKey)
+            && Objects.deepEquals(newMerkleValue, that.newMerkleValue)
+            && Objects.equals(childrenMerkleValues, that.childrenMerkleValues);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Arrays.hashCode(newMerkleValue), partialKey, childrenMerkleValues);
+    }
+
+    @Override
+    public String toString() {
+        return "PendingInsertUpdate{" +
+            "newMerkleValue=" + Arrays.toString(newMerkleValue) +
+            ", partialKey=" + partialKey +
+            ", childrenMerkleValues=" + childrenMerkleValues +
+            '}';
+    }
 }
