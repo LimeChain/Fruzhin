c9e205f09173911088200b5579d92f82d9998af0
osrib <oleksandr.sribnyi@limechain.tech>
2024-10-10 13:21:56 +0300

    Logic to support safe and unsafe behavior for local and public rpc (#553)

--- src/test/java/com/limechain/network/kad/KademliaServiceTest.java
+++ src/test/java/com/limechain/network/kad/KademliaServiceTest.java
@@ -2,26 +2,9 @@ package com.limechain.network.kad;
 
 import org.junit.jupiter.api.Test;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class KademliaServiceTest {
-
-    /**
-     * Test might throw UnknownHostException and fail if domain is no longer accessible
-     *
-     * @throws UnknownHostException
-     */
-    @Test
-    void dnsNodeToIp4_TransformDnsNode() throws UnknownHostException {
-        String bootNode = "/dns/p2p.0.polkadot.network/tcp/30333/p2p/12D3KooWHsvEicXjWWraktbZ4MQBizuyADQtuEGr3NbDvtm5rFA5";
-        InetAddress address = InetAddress.getByName("p2p.0.polkadot.network");
-        String expected = "/ip4/" + address.getHostAddress() + "/tcp/30333/p2p/12D3KooWHsvEicXjWWraktbZ4MQBizuyADQtuEGr3NbDvtm5rFA5";
-
-        assertEquals(expected, DnsUtils.dnsNodeToIp4(bootNode));
-    }
 
     @Test
     void dnsNodeToIp4_UnchangedAddress_ifInvalidDomain() {
