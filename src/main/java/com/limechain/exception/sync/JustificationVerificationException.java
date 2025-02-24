4bc9b3af6b220e30dfb05ece9d48d1790c5329c9
Hristiyan Mitov <hristiyan.mitov@limechain.tech>
2025-02-24 13:11:21 +0200

    Removed additional constructor from JustificationVerificationException.java

--- src/main/java/com/limechain/exception/sync/JustificationVerificationException.java
+++ src/main/java/com/limechain/exception/sync/JustificationVerificationException.java
@@ -4,8 +4,4 @@ public class JustificationVerificationException extends RuntimeException {
     public JustificationVerificationException(String message) {
         super(message);
     }
-
-    public JustificationVerificationException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
