83be1d6cc58533398f7891f53c684a7208c34349
Hristiyan Mitov <hristiyan.mitov@limechain.tech>
2025-02-28 11:47:28 +0200

    fixed pr comments.

--- src/main/java/com/limechain/beefy/state/BeefySession.java
+++ src/main/java/com/limechain/beefy/state/BeefySession.java
@@ -1,15 +1,15 @@
 package com.limechain.beefy.state;
 
 import com.limechain.beefy.dto.ValidatorSet;
-import lombok.Data;
+import lombok.Value;
 
 import java.math.BigInteger;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
-@Data
+@Value
 public class BeefySession {
-    private ValidatorSet validatorSet;
-    private Set<BigInteger> nonMandatoryBlockNumbers = Collections.synchronizedSortedSet(new TreeSet<>());
+    ValidatorSet validatorSet;
+    Set<BigInteger> nonMandatoryBlockNumbers = Collections.synchronizedSet(new HashSet<>());
 }
