ce796a956d34ffff1d4a1d1ad7486c33fa15d3e0
Hristiyan Mitov <hristiyan.mitov@limechain.tech>
2025-03-17 11:58:15 +0200

    Removed unnecessary methods withing commit.

--- src/main/java/com/limechain/consensus/beefy/BeefyService.java
+++ src/main/java/com/limechain/consensus/beefy/BeefyService.java
@@ -35,32 +35,6 @@ public class BeefyService {
     private final StateManager stateManager;
     private final BeefyState beefyState;
 
-    /**
-     * The threshold is determined as the numOfValidators - (numOfValidators - 1) / 3
-     *
-     * @return minimum required validators for finality.
-     */
-    private BigInteger getThreshold() {
-        BeefyAuthoritySet authoritySet = stateManager.getBeefyState().getAuthoritySet();
-
-        if (Objects.isNull(authoritySet)) {
-            log.warning("getThreshold: No authoritySet in BeefyState.");
-            return BigInteger.ZERO;
-        }
-
-        var validatorSize = authoritySet.getPublicKeys().size();
-
-        if (validatorSize == 0) {
-            log.warning("getThreshold: Validator set is empty.");
-            return BigInteger.ZERO;
-        }
-
-        var numOfValidators = BigInteger.valueOf(validatorSize);
-        var faulty = (numOfValidators.subtract(BigInteger.ONE)).divide(THRESHOLD_DENOMINATOR);
-
-        return numOfValidators.subtract(faulty);
-    }
-
     public void vote() {
         BeefyState beefyState = stateManager.getBeefyState();
         // Get the first session (round)
@@ -122,119 +96,31 @@ public class BeefyService {
         // TODO: Broadcast Vote Message
     }
 
+    /**
+     * The threshold is determined as the numOfValidators - (numOfValidators - 1) / 3
+     *
+     * @return minimum required validators for finality.
+     */
+    private BigInteger getThreshold() {
+        BeefyAuthoritySet authoritySet = stateManager.getBeefyState().getAuthoritySet();
 
-//    private void processJustification(SignedCommitment justification) {
-//
-//        //TODO: check for justification in db
-//
-//        BigInteger beefyFinalized = beefyState.getBeefyFinalized();
-//        if (Objects.isNull(beefyFinalized)) {
-//            throw new BeefyGenericException("Beefy finalized is not initialized yet.");
-//        }
-//
-//
-//        BigInteger blockNumber = justification.getCommitment().getBlockNumber();
-//        Map<BigInteger, BeefySession> found = Collections.emptyMap();
-//
-//        BigInteger nextDigest = beefyState.getNextDigest();
-//        if (Objects.isNull(nextDigest)) {
-//            beefyState.initializeNextDigest();
-//        }
-//
-//        if (blockNumber.compareTo(beefyFinalized) <= 0) {
-//            found = retrieveMandatoryBlockAndCreateBeefySession(blockNumber, blockNumber);
-//            if (found.isEmpty()) {
-//                return;
-//            }
-//        } else if (blockNumber.compareTo(beefyState.getNextDigest()) >= 0) {
-//            found = retrieveMandatoryBlockAndCreateBeefySession(blockNumber, beefyState.getNextDigest());
-//        }
-//
-//        Map.Entry<BigInteger, BeefySession> currentSession = determineCurrentSessionOnJustification(blockNumber, found);
-//        if (Objects.isNull(currentSession)) {
-//            return;
-//        }
-//
-//        BigInteger mandatoryBlock = found.isEmpty() ?
-//                currentSession.getKey() : found.keySet().iterator().next();
-//
-//        BeefySession beefySession = found.isEmpty() ?
-//                currentSession.getValue() : found.values().iterator().next();
-//
-//        //TODO: Validate justification
-//
-//        //TODO: Store justification in db if successfully verified
-//
-//        cleanUpSessionsAndRoundsOnJustification(currentSession, blockNumber);
-//
-//        LinkedHashMap<BigInteger, BeefySession> beefySessions = beefyState.getSessions();
-//        if (!found.isEmpty()) {
-//            // Store the newly found block and corresponding session, where authority change appeared
-//            beefySessions.put(mandatoryBlock, beefySession);
-//        }
-//
-//        //TODO: Update state
-//    }
-//
-//    private Map.Entry<BigInteger, BeefySession> determineCurrentSessionOnJustification(BigInteger blockNumber,
-//                                                                                       Map<BigInteger, BeefySession> found) {
-//
-//        LinkedHashMap<BigInteger, BeefySession> beefySessions = beefyState.getSessions();
-//        // Initialize the current session with the last possible session that we have
-//        Map.Entry<BigInteger, BeefySession> currentSession = beefySessions.lastEntry();
-//
-//        // If no new mandatory block and corresponding session are being found for the current block
-//        if (found.isEmpty()) {
-//            if (blockNumber.compareTo(beefyState.getBeefyFinalized()) <= 0) {
-//                log.warning(String.format("Block: %d has already been finalized.", blockNumber));
-//                return null;
-//            }
-//            // The mandatory block of the beefy session, that is greater than the one the block corresponds to
-//            Optional<BigInteger> nextSessionMandatoryBlockOpt = getNextSessionMandatoryBlock(blockNumber);
-//
-//            // If block is greater that the biggest mandatory block or next session is the first that exists
-//            if (nextSessionMandatoryBlockOpt.isEmpty()
-//                    || nextSessionMandatoryBlockOpt.get().equals(beefySessions.firstEntry().getKey())) {
-//
-//                log.warning(String.format("No session found for the specified block: %d", blockNumber));
-//                return null;
-//            }
-//
-//            BigInteger nextSessionMandatoryBlock = nextSessionMandatoryBlockOpt.get();
-//            //The beefy session, which the block corresponds to
-//            Optional<Map.Entry<BigInteger, BeefySession>> currentSessionOpt = getPreviousBeefySession(
-//                    nextSessionMandatoryBlock
-//            );
-//            if (currentSessionOpt.isEmpty()) {
-//                log.warning(String.format(
-//                        "No corresponding session was found for block: %d. " +
-//                                "Attempted to find session preceding mandatory block: %d. ",
-//                        blockNumber,
-//                        nextSessionMandatoryBlock
-//                ));
-//                return null;
-//            }
-//            currentSession = currentSessionOpt.get();
-//        }
-//        return currentSession;
-//    }
+        if (Objects.isNull(authoritySet)) {
+            log.warning("getThreshold: No authoritySet in BeefyState.");
+            return BigInteger.ZERO;
+        }
 
-//    private Optional<BigInteger> getNextSessionMandatoryBlock(BigInteger blockNumber) {
-//        return beefyState.getSessions().keySet().stream()
-//                .filter(key -> key.compareTo(blockNumber) > 0)
-//                .findFirst();
-//    }
-//
-//    private Optional<Map.Entry<BigInteger, BeefySession>> getPreviousBeefySession(BigInteger beefySessionKey) {
-//        Map.Entry<BigInteger, BeefySession> previousEntry = null;
-//        for (Map.Entry<BigInteger, BeefySession> entry : beefyState.getSessions().entrySet()) {
-//            if (entry.getKey().equals(beefySessionKey)) {
-//                return Optional.ofNullable(previousEntry);
-//            }
-//            previousEntry = entry;
-//        }
-//        return Optional.empty();
-//    }
+        var validatorSize = authoritySet.getPublicKeys().size();
+
+        if (validatorSize == 0) {
+            log.warning("getThreshold: Validator set is empty.");
+            return BigInteger.ZERO;
+        }
+
+        var numOfValidators = BigInteger.valueOf(validatorSize);
+        var faulty = (numOfValidators.subtract(BigInteger.ONE)).divide(THRESHOLD_DENOMINATOR);
+
+        return numOfValidators.subtract(faulty);
+    }
 
     private void cleanUpSessionsAndRoundsOnJustification(BeefySession currentSession,
                                                          BigInteger blockNumber) {
@@ -250,50 +136,6 @@ public class BeefyService {
             currentSessionRounds.keySet().removeIf(key -> key.getBlockNumber().compareTo(blockNumber) < 0);
         }
     }
-
-//    /**
-//     * Applied to all the blocks between beefyDigest and grandpa finalized.
-//     * Each found mandatory block with its corresponding beefy session is appended.
-//     */
-//    private void handleBeefySessionTransitions() {
-//        BigInteger nextDigest = beefyState.getNextDigest();
-//        BigInteger grandpaFinalized = beefyState.getGrandpaFinalized();
-//        LinkedHashMap<BigInteger, BeefySession> beefySessions = beefyState.getSessions();
-//
-//        if (Objects.isNull(nextDigest)) {
-//            beefyState.initializeNextDigest();
-//        }
-//
-//        if (Objects.isNull(grandpaFinalized)) {
-//            throw new BeefyGenericException("Grandpa finalized is not initialized yet.");
-//        }
-//
-//        while (nextDigest.compareTo(grandpaFinalized) <= 0) {
-//            //TODO: In kagome fetching header.
-//            Map<BigInteger, BeefySession> found = processConsensusMessages(
-//                    nextDigest,
-//                    beefySessions.isEmpty() ? beefyState.getBeefyGenesis() : nextDigest
-//            );
-//
-//            if (!found.isEmpty()) {
-//                beefySessions.putAll(found);
-//            }
-//
-//            nextDigest = nextDigest.add(BigInteger.ONE);
-//        }
-//        beefyState.setNextDigest(nextDigest);
-//
-//        // stop voting on first finalized session when there are more sessions
-//        BigInteger sessionMandatoryBlock = beefySessions.firstEntry().getKey();
-//        if (beefySessions.size() > 1 && sessionMandatoryBlock.compareTo(beefyState.getBeefyFinalized()) <= 0) {
-//            beefySessions.remove(sessionMandatoryBlock);
-//        }
-//
-//        //TODO: Start voting
-//
-//        //TODO: Consider if we already have beefy sessions and we do not have justification
-//        //      for the first mandatory block to fetch it.
-//    }
 
     /**
      * Examines BEEFY authority consensus messages, within grandpaFinalized currently known for Beefy + 1
