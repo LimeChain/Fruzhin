cd992335abc7583ad7d625c58f1a0effe5a3e392
Grigorov-Georgi <grigorov_georgi@yahoo.com>
2025-04-30 16:05:17 +0300

    fix: refactor from long to int casting of the authority index

--- src/main/java/com/limechain/consensus/babe/BabeService.java
+++ src/main/java/com/limechain/consensus/babe/BabeService.java
@@ -188,7 +188,7 @@ public class BabeService implements SlotChangeListener {
         Authority authority = stateManager.getEpochState()
                 .getCurrentEpochData()
                 .getAuthorities()
-                .get((int) digest.getAuthorityIndex()); // this is not self casting
+                .get(requireValidIntIndex(digest.getAuthorityIndex()));
 
         Schnorrkel.KeyPair keyPair = keyStore.getKeyPair(KeyType.BABE, authority.getPublicKey())
                 .map(keyStore::convertToSchnorrKeypair)
@@ -196,6 +196,14 @@ public class BabeService implements SlotChangeListener {
 
         updatedDigests[length] = DigestHelper.buildSealHeaderDigest(header, keyPair);
         return updatedDigests;
+    }
+
+    private int requireValidIntIndex(long index) {
+        if (index < 0 || index > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("Authority index out of valid int range: " + index);
+        }
+
+        return (int) index;
     }
 
     private List<ValidTransaction> produceBlockTransactions(Slot slot, Runtime runtime) {
