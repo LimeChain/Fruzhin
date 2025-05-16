197f077a6eae1fd2722a81f39089cfb6f8b29c13
Grigorov-Georgi <grigorov_georgi@yahoo.com>
2025-05-16 15:29:01 +0300

    chore: refactor

--- src/main/java/com/limechain/storage/block/tree/BlockTree.java
+++ src/main/java/com/limechain/storage/block/tree/BlockTree.java
@@ -181,7 +181,7 @@ public class BlockTree {
 
         BlockNode startBlockNode = getNode(startHash);
         if (startBlockNode == null) {
-            throw new BlockNodeNotFoundException("Start node not found");
+            throw new BlockNodeNotFoundException("Start node not found"); //TODO: Add break point
         }
 
         return accumulateHashesInDescendingOrder(endBlockNode, startBlockNode);
@@ -333,7 +333,7 @@ public class BlockTree {
 
         BlockNode parentBlockNode = getNode(parent);
         if (parentBlockNode == null) {
-            throw new BlockNodeNotFoundException("Start node not found: " + parent);
+            throw new BlockNodeNotFoundException("Start node not found: " + parent); //TODO: Add break point
         }
 
         BlockNode childBlockNode = getNode(child);
